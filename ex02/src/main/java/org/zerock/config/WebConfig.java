911b4a5b8c2f3adde24906f73a8f7e6fbfa4d88b
김재규 <o_opop9999@naver.com>
2023-07-26 17:53:39 +0900

    MVC 프로젝트 기본구성

---
+++ ex02/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,37 @@
+package org.zerock.config;
+
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class[] {RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected void customizeRegistration(
+		ServletRegistration.Dynamic registration) {
+			
+			registration.setInitParameter("throwExce[topnIfNoHandlerFound","true");
+			
+		}
+		
+	}
+
+
